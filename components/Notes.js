f4f73f61fe84bbedfa9d8c03d62626e029828c0b
Derese92 <deresee92@gmail.com>
2020-05-09 19:06:07 -0500

    final

--- components/Notes.js
+++ components/Notes.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
-import {
-    StyleSheet,
-} from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, ScrollView, View, Text, TouchableOpacity } from 'react-native';
 
 import Badge from 'components/Badge';
 import Separator from 'components/Separator';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 
-function Notes({ route }) {
+function Notes({ route: { params: { user } } }) {
+    const [state, setState] = React.useState({ notes: [], note: '' })
 
-    return (null);
+    const renderNote = note => {
+        return (
+            <View style={styles.rowContainer}>
+                <Text>{note}</Text>
+                <Separator />
+            </View>
+        )
+    }
+
+    const handleNote = (note) => {
+        setState(prev => ({ ...prev, note }))
+    }
+
+    const handleSubmit = () => {
+        setState(prev => ({ notes: prev.notes.concat(prev.note) }))
+    }
+
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <Badge {...user} />
+            <ScrollView>
+                {state.notes.map(note => renderNote(note))}
+            </ScrollView>
+            <View style={styles.footerContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    value={state.note}
+                    onChangeText={handleNote}
+                    placeholder='New Note'
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleSubmit}
+                >
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </View>
+            {/* <View style={{ height: 50, backgroundColor: 'black' }} /> */}
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
--- components/Repositories.js
+++ components/Repositories.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import {
-    StyleSheet,
-} from 'react-native';
+import { StyleSheet, ScrollView, ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
 
 import Separator from 'components/Separator';
 import Badge from 'components/Badge';
 
-function Repositories({ navigation, route }) {
-
-    return (null)
+function Repositories({ navigation: { navigate }, route: { params: { user } } }) {
+    const [state, setState] = React.useState({ list: [], loading: true, error: false })
+    const { list, loading, error } = state;
+    React.useEffect(() => {
+        fetch(user.repos_url)
+            .then(response => response.json())
+            .then(data => {
+                const list_elements = data.map((repo, index) => {
+                    return (
+                        <View key={index} style={styles.rowContainer}>
+                            <TouchableOpacity onPress={() => navigate('WEB', { url: repo.html_url })}>
+                                <Text style={styles.name}>{repo.name}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.description}>{repo.description ? repo.description : ''}</Text>
+                            <Separator />
+                        </View>
+                    )
+                })
+                setState(prev => ({ ...prev, loading: false, list: list_elements }))
+            })
+            .catch(error => setState(prevState => ({ ...prevState, loading: false, error: true })))
+    }, [])
+    return (
+        <ScrollView style={styles.container}>
+            <Badge {...user} />
+            {loading ? <ActivityIndicator size='large' /> : list}
+            {error && <Text>Something went Wrong!!</Text>}
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -32,7 +56,7 @@ const styles = StyleSheet.create({
     },
     description: {
         fontSize: 14,
-        paddingBottom: 5
+        paddingBottom: 10
     }
 });
 
